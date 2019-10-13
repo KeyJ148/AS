0f41d1a1dbaf1e835b38c0fd39aba94895d0bf23
Евгений Серов <kejik148@gmail.com>
2019-10-13 16:50:11 +0300

    Массивы байт для команд вынесены в константы Команды посылаются с использованием функции sendCommand

--- MHZ19b.cpp
+++ MHZ19b.cpp
@@ -4,19 +4,7 @@ MHZ19b::MHZ19b(int RX, int TX){
   swSerial = new SoftwareSerial(RX, TX);
   swSerial->begin(9600);
 
-  byte setrangeA_cmd[9] = {0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x13, 0x88, 0xCB};
-  unsigned char setrangeA_response[9]; 
-  swSerial->write(setrangeA_cmd,9);
-  swSerial->readBytes(setrangeA_response, 9);
-  byte setrangeA_crc = 0;
-  for (int setrangeA_i = 1; setrangeA_i < 8; setrangeA_i++) setrangeA_crc+=setrangeA_response[setrangeA_i];
-  setrangeA_crc = 255 - setrangeA_crc; setrangeA_crc++;
-  
-  if ( !(setrangeA_response[0] == 0xFF && setrangeA_response[1] == 0x99 && setrangeA_response[8] == setrangeA_crc) ) {
-    init = false;
-  } else {
-    init = true;
-  }
+  init = sendCommand(INIT_MAX_5000).valid;
 }
 
 MHZ19b::~MHZ19b(){
@@ -28,43 +16,17 @@ bool MHZ19b::getInit(){
 }
 
 bool MHZ19b::setABC(bool abc){
-  byte abcOn[9] =  {0xFF, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00, 0xE6};
-  byte abcOff[9] = {0xFF, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86};
-  unsigned char setrangeA_response[9]; 
-  swSerial->write((abc)? abcOn : abcOff, 9);
-  swSerial->readBytes(setrangeA_response, 9);
-  int setrangeA_i;
-  int setrangeA_crc = 0;
-  for (setrangeA_i = 1; setrangeA_i < 8; setrangeA_i++) setrangeA_crc+=setrangeA_response[setrangeA_i];
-  setrangeA_crc = 255 - setrangeA_crc;
-  setrangeA_crc += 1;
-  
-  if ( !(setrangeA_response[0] == 0xFF && setrangeA_response[1] == 0x99 && setrangeA_response[8] == setrangeA_crc) ) {
-    return false;
-  } else {
-    return true;
-  }
+  return sendCommand((abc)? ABC_ON : ABC_OFF).valid;
 }
 
 result<int> MHZ19b::getCO2(){
-  byte measure_cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
-  unsigned char measure_response[9]; 
-
-  //Узнаём концентрацию CO2 через UART
-  swSerial->write(measure_cmd,9);
-  swSerial->readBytes(measure_response, 9);
-  byte crc = 0;
-  for (int i = 1; i < 8; i++) crc+=measure_response[i];
-  crc = 255 - crc; crc++;
-  if ( !(measure_response[0] == 0xFF && measure_response[1] == 0x86 && measure_response[8] == crc) ) {
-    return result<int>(0, false);
-  } 
+  result<vector<byte>> response = sendCommand(INIT_MAX_5000);
   
-  unsigned int responseHigh = (unsigned int) measure_response[2];
-  unsigned int responseLow = (unsigned int) measure_response[3];
+  unsigned int responseHigh = (unsigned int) response.val[2];
+  unsigned int responseLow = (unsigned int) response.val[3];
   unsigned long ppm = (256*responseHigh) + responseLow;
 
-  return result<int>(ppm, true);
+  return result<int>(ppm, response.valid);
 }
 
 result<vector<byte>> MHZ19b::sendCommand(const byte command[]){
@@ -74,15 +36,15 @@ result<vector<byte>> MHZ19b::sendCommand(const byte command[]){
   unsigned char response[9];
   swSerial->readBytes(response, 9);
 
-  return result<vector<byte>>(vector<byte>(9, response), checkResponse(response));
+  return result<vector<byte>>(vector<byte>(9, response), checkResponse(command[2], response));
 }
 
-bool MHZ19b::checkResponse(const byte response[]){
+bool MHZ19b::checkResponse(byte commandID, const byte response[]){
   if (sizeof(response) != 9) return false;
 
   byte crc = 0;
   for (int i = 1; i < 8; i++) crc += response[i];
   crc = 255 - crc; crc++;
   
-  return !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc);
+  return !(response[0] == 0xFF && response[1] == commandID && response[8] == crc);
 }
