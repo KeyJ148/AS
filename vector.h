c2fb13b579d49ae6c4e35e2d6e88c031302a7b8f
Евгений Серов <kejik148@gmail.com>
2019-10-14 08:08:17 +0300

    Реализация шаблонного класса перенесена в заголовочный файл

--- vector.h
+++ vector.h
@@ -5,14 +5,33 @@
 template<class T> 
 class vector{
 public:
-  vector(int);
-  vector(int, T);
-  vector(int, const T []);
-  vector(const vector&);
-  ~vector();
+
+  vector(int sizeArray) : sizeArray(sizeArray){
+    val = new T[sizeArray];
+  }
   
-  int size();
-  T& operator[](int);
+  vector(int sizeArray, T init) : vector(sizeArray){
+    for(int i=0; i<sizeArray; i++) val[i] = init;
+  }
+  
+  vector(int sizeArray, const T init[]) : vector(sizeArray){
+    for(int i=0; i<sizeArray; i++) val[i] = init[i];
+  }
+  vector(const vector<T>& copy) : vector(copy.size()){
+    for(int i=0; i<sizeArray; i++) val[i] = copy[i];
+  }
+  
+  ~vector(){
+    delete[] val;
+  }
+  
+  int size(){
+    return sizeArray;
+  }
+  
+  T& operator[](int pos){
+    return val[pos];
+  }
   
 private:
   T* val;  
