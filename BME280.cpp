a3a78f49581dd80465548b5a5ab6f01ae02efc81
Евгений Серов <kejik148@gmail.com>
2019-10-17 18:14:11 +0300

    В BME280 улучшена проверка показаний датчиков

--- BME280.cpp
+++ BME280.cpp
@@ -11,15 +11,15 @@ bool BME280::getInit(){
 
 result<float> BME280::getTemperature(){
   float t = sensor.readTemperature();
-  return result<float>(t, !isnan(t));
+  return result<float>(t, !isnan(t) && t > -100 && t < 100);
 }
 
 result<float> BME280::getHumidity(){
   float h = sensor.readHumidity();
-  return result<float>(h, !isnan(h) && h != 0);
+  return result<float>(h, !isnan(h) && h > 1 && h < 99);
 }
 
 result<float> BME280::getPressure(){
   float p = sensor.readPressure() / 100 * MBR_TO_MMHG;
-  return result<float>(p, !isnan(p) && p != 0);
+  return result<float>(p, !isnan(p) && p > 640 && p < 810);
 }
